d27bc33733d0b6b82886fe8ec86dd93d664efff2
Felipe Frid Buniac <febuniac@gmail.com>
2026-04-05 13:11:28 -0300

    Add src/utils/encryption.js

---
+++ src/utils/encryption.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+const ALGORITHM = 'aes-256-gcm';
+const KEY = Buffer.from(process.env.ENCRYPTION_KEY || '', 'hex');
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(ALGORITHM, KEY, iv);
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  const tag = cipher.getAuthTag().toString('hex');
+  return iv.toString('hex') + ':' + encrypted + ':' + tag;
+}
+
+function decrypt(data) {
+  const [ivHex, encrypted, tagHex] = data.split(':');
+  const decipher = crypto.createDecipheriv(ALGORITHM, KEY, Buffer.from(ivHex, 'hex'));
+  decipher.setAuthTag(Buffer.from(tagHex, 'hex'));
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+module.exports = { encrypt, decrypt };
