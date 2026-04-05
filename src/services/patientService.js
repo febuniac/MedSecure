f3cc303d1aab816d9d9e4f0857401fed488ce8c2
Felipe Frid Buniac <febuniac@gmail.com>
2026-04-05 13:11:22 -0300

    Add src/services/patientService.js

---
+++ src/services/patientService.js
@@ -0,0 +1,19 @@
+const db = require('../models/db');
+const { encrypt, decrypt } = require('../utils/encryption');
+
+class PatientService {
+  static async list(filters, user) {
+    return db('patients').where({ provider_id: user.provider_id }).select('id', 'first_name', 'last_name', 'dob', 'created_at');
+  }
+  static async getById(id, user) {
+    const patient = await db('patients').where({ id }).first();
+    if (patient) { patient.ssn = decrypt(patient.ssn_encrypted); }
+    return patient;
+  }
+  static async create(data, user) {
+    data.ssn_encrypted = encrypt(data.ssn);
+    delete data.ssn;
+    return db('patients').insert(data).returning('*');
+  }
+}
+module.exports = PatientService;
